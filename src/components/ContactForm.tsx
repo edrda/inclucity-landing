6c905f957b440d787c20f7061b9e8708c49a8c8c
eduarda <e.qlourennco@gmail.com>
2025-08-28 20:11:37 -0300

    🔥first commit

---
+++ src/components/ContactForm.tsx
@@ -0,0 +1,7 @@
+<form action="https://formspree.io/f/meoleewe" method="POST" className="space-y-4">
+  <input type="text" name="name" required placeholder="Seu nome" />
+  <input type="email" name="email" required placeholder="E-mail" />
+  <input type="tel" name="whatsapp" required placeholder="(xx) xxxxx-xxxx" />
+  <textarea name="message" placeholder="Mensagem"></textarea>
+  <button type="submit">Enviar</button>
+</form>
